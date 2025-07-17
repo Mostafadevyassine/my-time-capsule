04c01a274169e7f15d97c81e2bcf424b48aca8ae
Mostafa Yassine <mostafadevyassine@users.noreply.github.com>
2025-07-17 23:26:24 +0300

    initial commit

---
+++ database/migrations/2025_07_17_200246_create_capsules_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capsules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('message');
+            $table->datetime('reveal_date');
+            $table->string('country')->nullable();
+            $table->enum('mood',['happy','sad','Excited','Angry','Lonely','Tired','Bored','Calm'])->nullable();
+            $table->enum('privacy',['public','private']);
+            $table->boolean('is_surprise')->default(false);
+            $table->boolean('is_revealed')->default(false)->unique();
+            $table->timestamps();
+        });
+
+
+        Schema::create('attachements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('capsule_id')->references('id')->on('capsules')->onDelete('cascade');
+            $table->string('file_name');
+            $table->timestamps();
+        });
+
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('capsules');
+    }
+};
